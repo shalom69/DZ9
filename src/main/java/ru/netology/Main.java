1b634c0605220e1a8d96c8709bf85f4095830408
shalom69 <108269015+shalom69@users.noreply.github.com>
2022-08-30 22:03:46 +0300

    mod

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,16 +3,16 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         Post post = new Post();
-        post.name = "Александр";
-        post.passport = "7416 № 816544";
-        post.patronymic = "Александрович";
-        post.phone = "+7 (800)-555-35-35";
-        post.surname = "Воронович";
+        post.name = "Николай";
+        post.passport = "5560 № 7256425";
+        post.patronymic = "Петрович";
+        post.phone = "+7 (995)-783-39-81";
+        post.surname = "Антонович";
         post.subscription = true;
 
         post.birthday = new FormDate();
-        post.birthday.day = 6;
-        post.birthday.month = 5;
-        post.birthday.year = 1993;
+        post.birthday.day = 13;
+        post.birthday.month = 4;
+        post.birthday.year = 1994;
     }
 }
