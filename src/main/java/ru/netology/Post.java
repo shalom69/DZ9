7f6fd23d13a8ff2fcb2a41cb2c7d7ad9c62e9b84
shalom69 <shalomaevanton@gmail.com>
2022-08-30 21:35:25 +0300

    DZ9

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public java.lang.String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
